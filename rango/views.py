633b410846066f38f81f93646b679349709ba06f
Jaroslav Sak <2094679s@student.gla.ac.uk>
2018-01-18 21:14:26 +0000

    Passed Test!

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 def index(request):
    # Construct a dictionary to pass to the template engine as its context.
    # Note the key boldmessage is the same as {{boldmessage}} in the template!
-   context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupacake!"}
+   context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupacake!'}
    return render(request, 'rango/index.html', context=context_dict) 
 def about(request):
-   context_dict = {'boldmessage':"This is a cat"}
+   context_dict = {'boldmessage':'This tutorial has been put together by <Jaroslav Sak>'}
    return render(request, 'rango/about.html', context=context_dict)
 
 # Create your views here.
