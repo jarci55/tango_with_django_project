d7d1f838978cca3aa43e0bb6b7c84545765fec6b
Jaroslav Sak <2094679s@student.gla.ac.uk>
2018-01-18 21:44:23 +0000

    6.2 done!

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 def index(request):
    # Construct a dictionary to pass to the template engine as its context.
    # Note the key boldmessage is the same as {{boldmessage}} in the template!
-   context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupacake!'}
-   return render(request, 'rango/index.html', context=context_dict) 
+ 
+   category_list = Category.objects.order_by('-likes')[:5]
+   context_dict = {'categories':category_list}
+   return render(request, 'rango/index.html', context_dict)
+
 def about(request):
    context_dict = {'boldmessage':'This tutorial has been put together by <Jaroslav Sak>'}
    return render(request, 'rango/about.html', context=context_dict)
