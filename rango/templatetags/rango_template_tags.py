98d7b45587f388599fa13b33e164789890876ef8
Jaroslav Sak <2094679s@student.gla.ac.uk>
2018-01-29 16:45:28 +0000

    9.7 Completed ... Login and Registration working!

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.inclusion_tag('rango/cats.html')
 def get_category_list(cat=None):
-    return {'cats': Category.objects.all(),
-    'act_cat': cat}
+     return {'cats': Category.objects.all(),
+             'act_cat': cat}
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -98,6 +98,12 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -108,6 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length':6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
